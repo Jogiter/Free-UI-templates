8315dc4e82df091d0f27275aeeb6b70dc9db52fe
jogiter <jogiter.g@gmail.com>
2022-04-18 18:27:32 +0800

    feat: add site layout

---
+++ layouts/site.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "NuxtJs UI kit | Free UI kit built with Vuetify",
+      meta: [
+        {
+          hid: "description",
+          name: "NuxtJs UI kit",
+          content: "NuxtJs UI kit is the best way"
+        }
+      ]
+    };
+  },
+};
+</script>
+
+<style></style>
