cee46866960339167dde5b93592d223c1e597f4f
jogiter <jogiter.g@gmail.com>
2022-04-18 18:28:02 +0800

    feat: add home page

---
+++ pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Header />
+    <Banner />
+    <Feature1 />
+    <Portfolio />
+    <Feature2 />
+    <C2a1 />
+    <PortfolioText />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Header: () => import("@/modules/Header"),
+    Banner: () => import("@/modules/Banner"),
+    Feature1: () => import("@/modules/Feature1"),
+    Portfolio: () => import("@/modules/Portfolio"),
+    Feature2: () => import("@/modules/Feature2"),
+    C2a1: () => import("@/modules/C2a1"),
+    PortfolioText: () => import("@/modules/PortfolioText"),
+  }
+}
+</script>
+
+<style>
+
+</style>
